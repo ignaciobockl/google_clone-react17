736ecf46600b2023c0a531301bb81f3da5840a26
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-04-30 15:58:49 -0300

    Router implementation of 'react-router-dom'

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 
@@ -8,6 +9,8 @@ import './index.css';
 
 
 ReactDOM.render(
-    <App />,
+    <Router>
+        <App />
+    </Router>,
     document.getElementById('root')
 );
