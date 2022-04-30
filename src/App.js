2c298d0a1c92cb4a6378e223a261d34b35f2bec7
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-04-30 15:25:15 -0300

    dark-theme settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+// import { Footer } from './components/Footer';
+// import { Navbar } from './components/Navbar';
+// import { Routes } from './components/Routes';
 
 
 
 const App = () => {
-  return (
-    <div>
-        <h1>App</h1>
-    </div>
-  )
+
+    const [ darkTheme, setDarkTheme ] = useState(false);
+
+    return (
+      <div className={ darkTheme ? 'dark' : '' }>
+          <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+              {/* <Navbar />
+              <Routes />
+              <Footer /> */}
+          </div>
+      </div>
+    )
 }
 
 export default App;
