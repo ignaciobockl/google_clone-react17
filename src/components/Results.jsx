f579586a63215ac137763963f66268ecf1069468
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-05-02 21:50:29 -0300

    update case /news and /videos

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -73,12 +73,12 @@ export const Results = () => {
                                     <p className='textlg dark:text-blue-300 text-blue-700'>
                                         { title }
                                     </p>
-                                    <div className='flex gap-4'>
-                                        <a href={source?.href} target='_blank' rel='noreferrer' className='hover:underline'>
-                                            {source?.href}
-                                        </a>
-                                    </div>
                                 </a>
+                                <div className='flex gap-4'>
+                                    <a href={source?.href} target='_blank' rel='noreferrer' className='hover:underline'>
+                                        {source?.href}
+                                    </a>
+                                </div>
                             </div>
                         ))
                     }
@@ -91,7 +91,11 @@ export const Results = () => {
                     {
                         results?.map((video, index) => (
                             <div key={index} className='p-2'>
-                                <ReactPlayer url={video.additional_links?.[0].href} controls width='355px' height='200px'/>
+                                {
+                                    video?.additional_links?.[0].href && 
+                                        <ReactPlayer url={video.additional_links?.[0].href} controls width='355px' height='200px'/>
+                                }
+                                {/* <ReactPlayer url={video.additional_links?.[0].href} controls width='355px' height='200px'/> */}
                             </div>
                         ))
                     }
